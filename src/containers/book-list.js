ee46cfb9c4dfc5adcc307eaebe64729291f32c2f
Matthew Hurewitz <matthew.hurewitz@bestbuy.com>
2017-09-07 21:12:34 -0500

    Mapped actionCreator to state

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 
 class BookList extends Component {
@@ -29,4 +30,7 @@ function mapStateToProps(state) {
     };
 }
 
-export default connect(mapStateToProps)(BookList);
+function mapDispatchToProps(dispatch) {
+    return bindActionCreators( {selectBook: selectBook}, dispatch);
+}
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
